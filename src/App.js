4fea87b0956e11e14b6689f172dd37ebd28059f0
Prasun117 <57390215+Prasun117@users.noreply.github.com>
2021-12-04 02:23:40 +0530

    layout completed

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { Grid } from "@material-ui/core";
 import { useState } from "react";
 import { HeaderComponent } from "./HeaderComponent";
 import { Footer } from "./Footer";
+import { SubHeader } from "./SubHeader";
 export default function App() {
   const [drawerToggle, setDrawerToggle] = useState(false);
   return (
     <div className="App">
       <HeaderComponent />
-      <div className="test"></div>
-      <div className="test"></div>
-      <div className="test"></div>
-      <div className="test"></div>
-      <div className="test"></div>
-      <div className="test"></div>
-      <div className="test"></div>
-      <div className="test"></div>
+      <SubHeader />
+      <div className="Content"></div>
+
       <Footer />
     </div>
   );
---
+++ src/SubHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SubHeader = (props) => {
+  return (
+    <div className="sub-header">
+      <div className="sub-header-title">Popular tiles</div>
+    </div>
+  );
+};
